723d3bb1e54968a2c5387d6302521e49fb4382f7
Bruno Montebello <brunormontebello@hotmail.com>
2022-02-22 21:52:42 -0300

    Add more test for invalid input

--- spec/foo_bar_spec.rb
+++ spec/foo_bar_spec.rb
@@ -5,8 +5,19 @@ describe Object, '#foo_bar_rule' do
   context "when input is not a number" do
     let(:invalid_input) { 'a' }
 
-    it 'expect to raise an error' do
+    it 'expect to raise "Not a number ERROR"' do
         expect { Object.foo_bar_rule(invalid_input) }.to raise_error
+    end
+  end
+end
+
+describe Object, '#foo_bar' do
+
+  context "when input is not a number" do
+    let(:invalid_input) { nil }
+
+    it 'expect to raise "Not a number ERROR"' do
+        expect { Object.foo_bar(invalid_input) }.to raise_error
     end
   end
 end
